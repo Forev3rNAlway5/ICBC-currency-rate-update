2eca36c51093cc8a847ac3f72af81ec6405bcdf7
Jeremy Shi <shijiehao1996@gmail.com>
2023-05-15 13:41:19 +0800

    add

---
+++ getcurrency.py
@@ -0,0 +1,38 @@
+import requests
+import re
+from datetime import datetime
+import pytz
+import math
+import time
+import argparse
+
+
+
+currency_dic = {"GBP": "英镑", "HKD": "港币", "USD": "美元", "CHF": "瑞士法郎", "SGD": "新加坡元", "PKR": "巴基斯坦卢比", "SEK": "瑞典克朗", "DKK": "丹麦克朗", "NOK": "挪威克朗", "JPY": "日元", "CAD": "加拿大元", "AUD": "澳大利亚元", "MYR": "林吉特", "EUR": "欧元", "RUB": "卢布", "MOP": "澳门元", "THB": "泰国铢", "NZD": "新西兰元", "ZAR": "南非兰特", "KZT": "哈萨克斯坦 坚戈", "KRW": "韩元"}
+
+def get_rate():
+    source = requests.get("http://www.icbc.com.cn/ICBCDynamicSite/Optimize/Quotation/QuotationListIframe.aspx").text
+
+    currency_rate = {}
+
+    for currency in currency_dic:
+        currency_rate[currency] = re.findall(r'(?<=width="14%">).*?(?=</td>)', re.search(r'(?<=' + currency_dic[currency] + '\(' + currency + '\)</td><td class="tdCommonTableData" align="right" valign="middle").*(?=</td>)', source).group(0))
+
+    return currency_rate
+
+def current_time():
+    now = datetime.now(pytz.timezone('Asia/Shanghai'))
+    return now.strftime("%m/%d/%Y, %H:%M:%S")
+
+def output(output_currency,rate):
+    print(rate[output_currency][2])
+
+def main():
+    parser = argparse.ArgumentParser(description="Get exchange rate from ICBC.")
+    parser.add_argument("--sell", type=str, required=False)
+    args = parser.parse_args()
+    rate = get_rate()
+    output(args.currency,rate)
+
+if __name__ == "__main__":
+    main()
